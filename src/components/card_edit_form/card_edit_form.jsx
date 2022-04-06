a71017024abf50da8639045c6bfecf6ed1e2e73b
Hihyu <4up456@naver.com>
2022-04-06 13:27:10 +0900

    preview UI완성

---
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -0,0 +1,6 @@
+import styles from './card_edit_form.module.css';
+import React from 'react';
+
+const CardEditForm = ({ card }) => <h1>{card.id}</h1>;
+
+export default CardEditForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react/cjs/react.development';
+import { useEffect, useState } from 'react/cjs/react.development';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
 import Header from '../header/header';
@@ -8,6 +8,41 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'dark',
+      email: 'ellie@gmail.com',
+      title: 'Software Engineer',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'light',
+      email: 'ellie@gmail.com',
+      title: 'Software Engineer',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+    {
+      id: '3',
+      name: 'Ellie',
+      company: 'Samsung',
+      theme: 'colorful',
+      email: 'ellie@gmail.com',
+      title: 'Software Engineer',
+      message: 'go for it',
+      fileName: 'ellie',
+      fileURL: null,
+    },
+  ]);
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
